557b4820dbe6719e0621be43d2798f9d880e66dc
monoharada <harada@monosus.co.jp>
2021-06-24 19:45:29 +0900

    task add

--- pages/task-page.js
+++ pages/task-page.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import { useEffect } from 'react'
 import Layout from '../components/Layout'
 import Link from 'next/link';
+import { getAllTasksData } from '../lib/tasks';
+import Task from '../components/Task'
+import useSwr from 'swr'
 
-const taskPage = () => {
+const fetcher = (url) => fetch(url).then((res) => res.json())
+const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task/`
+
+const taskPage = ({ staticfiltredTasks }) => {
+
+  const { data: tasks, mutate } = useSwr(apiUrl, fetcher, {
+    initialData: staticfiltredTasks
+  })
+  const filteredTasks = tasks?.sort(
+    (a, b) => new Date(b.created_at) - new Date(a.created_at)
+  )
+  useEffect(() => {
+    mutate()
+  }, [])
+
   return (
     <Layout title="task-page">
+      <ul>
+        {filteredTasks &&
+          filteredTasks.map((task) =>
+            <Task key={task.id} task={task} />
+          )
+        }
+      </ul>
       <Link href="/main-page">
         <div className="flex cursor-pointer mt-12">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -18,3 +42,14 @@ const taskPage = () => {
 }
 
 export default taskPage
+
+export async function getStaticProps() {
+  const staticfiltredTasks = await getAllTasksData()
+
+  return {
+    props: {
+      staticfiltredTasks
+    },
+    revalidate: 3,
+  }
+}
