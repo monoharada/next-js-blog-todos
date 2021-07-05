11dd508425b5bb9f04b1344f6968fe77386cc760
monoharada <harada@monosus.co.jp>
2021-07-05 03:08:38 +0000

    l-33

--- components/Task.js
+++ components/Task.js
@@ -1,15 +1,18 @@
 import Link from 'next/link'
 import Cookie from 'universal-cookie'
+import { useContext } from 'react'
+import { StateContext } from '../context/StateContext'
+
 
 const cookie = new Cookie()
 
 export default function Task({ task, taskDeleted }) {
-
+  const { setSelectedTask } = useContext(StateContext)
   const deleteTask = async () => {
     await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/tasks/${task.id}`, {
       method: "DELETE",
       headers: {
-        "Content-type": "application/json",
+        "Content-Type": "application/json",
         Authorization: `JWT ${cookie.get("access_token")}`
       }
     }).then((res) => {
@@ -31,6 +34,11 @@ export default function Task({ task, taskDeleted }) {
         </span>
       </Link>
       <div className="float-right ml-20">
+        <svg
+          onClick={() => setSelectedTask(task)}
+          xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 float-left cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+        </svg>
         <svg onClick={deleteTask} xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
         </svg>
